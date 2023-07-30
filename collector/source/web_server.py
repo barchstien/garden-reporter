4dae0d7574a4b52aa8c9f7f27dd381ec238dc957
bastien <bastien.auneau@tsat.net>
2023-07-30 12:47:38 +0200

    Add more paths to poll and update

--- collector/source/web_server.py
+++ collector/source/web_server.py
@@ -1,19 +1,48 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import re
+import re, json
+import urllib.parse
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
 
-        # Read content from index.html file and send it as the response
-        with open('source/index.html', 'rb') as file:
-            content = file.read()
-            content = re.sub(b'{{period_value}}', b'2', content)
-            content = re.sub(b'{{start_time_value}}', b'20:03', content)
-            content = re.sub(b'{{duration_value}}', b'45', content)
-            self.wfile.write(content)
+            # Read content from index.html file and send it as the response
+            with open('source/index.html', 'rb') as file:
+                content = file.read()
+                content = re.sub(b'{{period_value}}', b'2', content)
+                content = re.sub(b'{{start_time_value}}', b'20:03', content)
+                content = re.sub(b'{{duration_value}}', b'45', content)
+                self.wfile.write(content)
+        
+        elif self.path == '/poll':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {'question': 'What is your favorite color?', 'options': ['Red', 'Blue', 'Green']}
+            json_data = json.dumps(data)
+            self.wfile.write(json_data.encode())
+        
+        elif self.path.startswith('/update'):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            query_components = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+            period_value = query_components.get('period_value', [''])[0]
+            start_time_value = query_components.get('start_time_value', [''])[0]
+            duration_value = query_components.get('duration_value', [''])[0]
+            water_liter_value = query_components.get('water_liter_value', [''])[0]
+
+            response_message = f'Period: {period_value}<br>'
+            response_message += f'Start Time: {start_time_value}<br>'
+            response_message += f'Duration: {duration_value}<br>'
+            response_message += f'Water Liter: {water_liter_value}<br>'
+            self.wfile.write(response_message.encode())
+
+        else:
+            self.send_error(404, "File Not Found")
 
 def run_server(port=8000):
     server_address = ('', port)
