0f242b856b58d7648ad07c8004f298c7d3987559
bastien <bastienauneau@gmail.com>
2024-06-24 17:26:50 +0000

    Better calib, set max size for container log in restart script

--- collector/restart_container.sh
+++ collector/restart_container.sh
@@ -4,4 +4,4 @@ NAME="garden-collector"
 
 docker container stop $NAME
 docker container rm $NAME
-docker run -d --restart always --name $NAME --group-add dialout --network host --env-file env_file --device=/dev/ttyUSB0 garden-collector
+docker run -d --restart always --name $NAME --group-add dialout --network host --env-file env_file --device=/dev/ttyUSB0 --log-opt max-size=50m garden-collector
