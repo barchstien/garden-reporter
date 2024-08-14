04971e51b1c5db9fee9c485553e7eaa118e0faaa
bastien <bastien.auneau@tsat.net>
2024-08-14 17:26:41 +0200

    Add scripts to compile/upload/serial to arduino from local

---
+++ water-control/serial_monitor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e 
+set -o pipefail
+
+PATH_ARDUINO_CLI="/home/bastien/while-true/common/arduino-ide_2.3.2_Linux_64bit/resources/app/lib/backend/resources/arduino-cli"
+
+# TODO add -u and specify port to upload to
+#$PATH_ARDUINO_CLI compile -b arduino:samd:nano_33_iot --output-dir ./bin/  ./water-control/
+$PATH_ARDUINO_CLI monitor -p /dev/ttyACM0
+
+# copy bin to rpi
+#scp bin/water-control.ino.bin bastien@192.168.1.133:/home/bastien/upload/
+# upload to board
+#ssh bastien@192.168.1.133 "cd /home/bastien;./upload.sh"
+
+# serial monitor
+#ssh bastien@192.168.1.133 "cd /home/bastien;./bin/arduino-cli monitor -p /dev/ttyACM0"
