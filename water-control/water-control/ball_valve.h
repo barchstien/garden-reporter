cb9dee0b213fec1e499fa4895894222ddb71ec44
bastien <bastien.auneau@tsat.net>
2025-03-09 00:43:33 +0100

    valve_t is now ball_valve_t, ni more pulses and retry and flow feedback

--- water-control/water-control/ball_valve.h
+++ water-control/water-control/ball_valve.h
@@ -2,80 +2,14 @@
 
 #include "pin.h"
 
-/**
- * From forums, latched solenoid valve need 20 to 50 msec puls
- * delay() is running @150%
- * 50 msec * 1.5 = 75 msec   1st fails
- * 40 msec * 1.5 = 60 msec   1st fails
- * 30 msec * 1.5 = 45 msec   No work
- * 20 msec * 1.5 = 30 msec   No work
- *                100 msec   1st fails, many fails after
- * 
- * Looks like 1st water fails after arduino is prog with USB ?
- * After 30+ tries (with different delays) :
- *  - 75 msec looks good
- *  - doesn't fails on 1st cycle anymore
- *  - no relation with USB prog
- * --> should wires we water protected ?
- * 
- * Few days later... impossible to open the valve
- * Two suspects : 
- *  1. corrosion on wires
- *  2. got to re-try open/close and log any problem
- * 
- * Wires are now twisted together and clamped
- * Been having an infinite loopn trying to close in vain !
- * --> Increase pulse duration ! Never had that before
- * Use 1000 msec, like before, was reliable
- * No much gain to have battery wise, because system would exhasut battery in few days only anyways
- * |--> Doesn't help ! Still can't close valve
- * |--> must be due to new wire twist + clamp
- * |--> Attach wires and solder
- * 
- * |--> Order of what to do
- *   1. clean by openinig bleeder
- *   2. clean by opening solenoid
- *   3. clean by opening valve body
- *   4. buy new valve (solenoid alone abd valve + solenoid cost the same...)
- * 
- *  None if the above (1. -> 4.) made it work
- *  |---> Working thanks to :
- *  I.  pressure reductor before valve
- *  II. 1000 msec pulse
- * 
- * 11/08/2024
- * Changed solenoid
- *   800 msec OK
- *   400      OK
- *   200      OK
- *   100      OK
- * ---> now using 100 msec
- * 
- * 22/08/2024 Manual water 30min
- * Web reports 109L, counter 126L
- * |--> 109 * 1.16 = 126.44 L
- * |--> Measure again, see if it's the same factor
- *      ... else it means measurement in unreliable...
- * Also, current factor for 1 pulse is 0.00245 L
- * 0.00245 * 1.16 = 0.002842
- * Knowing that 1 fluid ounce (fl oz) is 28.413 mL => 0.02841
- * Could it be that 1 pulse => 1/10 fl oz ?
- * 
- * 27/08/2024
- * Again... 5 pulses, tried 6 times, can't open water...
- * Could it be pulse length ? 
- * |--> doing retry and increasing pulse length
-*/
-#define VALVE_PULSE_MSEC 100
-#define VALVE_PHASE_OPEN 1
-#define VALVE_PHASE_CLOSE 0
-
-#define MAX_WATER_ON_TRY 50
-#define VALVE_PULSE_STEP_MSEC 20
+// Set Driver phase, WARNING RISK of inverse polarity !!
+#define BALL_VALVE_POLARITY 1 //<--- TODO check with multimeter
+#define BALL_VALVE_ON 1
+#define BALL_VALVE_OFF 0
 
 /**
 * Controle DRV8874 Single Brushed DC Motor Driver Carrier
-* Which is used as a driver for the single coil 9V latching relay
+* Which is used as a driver a ball valve
 * mini board : https://www.pololu.com/product/4035
 * It uses 2 PIN to control
 * Enable | Phase | OUT1 | OUT2 |
@@ -86,89 +20,35 @@
 * ------------------------------
 *    1   |   0   |   L  |  H   |
 * ------------------------------
+* Ball valves is closed by default
+* It opens when applying DC 9-24V
+* and remains open until DC is removed
+* WARNING PHASE is set once and for all to avoid reverse polarity !!
 */
-struct valve_t
+struct ball_valve_t
 {
-  /**
-   * @param WATER_ON_MIN_TRIG min number of flow trig to consider water as ON
-   * @param WATER_ON_MIN_TRIG_DELAY_MSEC delay to wait for flow triggers/pulses
-  */
-  valve_t(int WATER_ON_MIN_TRIG, int WATER_ON_MIN_TRIG_DELAY_MSEC) :
-    WATER_ON_MIN_TRIG_(WATER_ON_MIN_TRIG),
-    WATER_ON_MIN_TRIG_DELAY_MSEC_(WATER_ON_MIN_TRIG_DELAY_MSEC)
-  {}
-
   /**
    * @param flow_cnt_ptr pointer to flow pulse counter, to check that water flows or not
   */
-  void init(uint32_t* flow_cnt_ptr)
+  void init()
   {
-    flow_cnt_ptr_ = flow_cnt_ptr;
     pinMode(VALVE_ENABLE, OUTPUT);
+    pinMode(VALVE_ENABLE, BALL_VALVE_OFF);
     pinMode(VALVE_PHASE, OUTPUT);
-    delay(250);
-    int pulse_applied_cnt = 0;
-    water_off(&pulse_applied_cnt);
+    pinMode(VALVE_PHASE, BALL_VALVE_POLARITY);
   }
 
-  /**
-   * @param num_of_pulse that was applied
-   * @return true if valve opened and water started flowing, else false
-  */
-  bool water_on(int* num_of_pulse)
+  void water_on() 
   {
-    // Loop until water is on or max try occured
-    int32_t flow_cnt_tmp = -1;
-    unsigned int cnt = 0;
-    while (flow_cnt_tmp == -1 || (*flow_cnt_ptr_ <= (flow_cnt_tmp + WATER_ON_MIN_TRIG_) && cnt < MAX_WATER_ON_TRY))
-    {
-      // pulse increases by VALVE_PULSE_STEP_MSEC at every try
-      water_on_(VALVE_PULSE_MSEC + cnt * VALVE_PULSE_STEP_MSEC);
-      flow_cnt_tmp = *flow_cnt_ptr_;
-      // wait a bit ot be sure water does flow
-      delay(WATER_ON_MIN_TRIG_DELAY_MSEC_);
-      cnt ++;
-    }
-    // also return number of pulses used to caller
-    *num_of_pulse = cnt;
-    if (*flow_cnt_ptr_ <= (flow_cnt_tmp + WATER_ON_MIN_TRIG_))
-    {
-      // failed to start water
-      return false;
-    }
-    is_on_ = true;
-    return true;
+    digitalWrite(VALVE_ENABLE, BALL_VALVE_ON);
   }
 
-  /**
-   * @param num_of_pulse that was applied
-   * @return true if valve closed and water stopped flowing, else false
-  */
-  bool water_off(int* num_of_pulse)
+  bool water_off()
   {
-    int32_t flow_cnt_tmp = -1;
-    unsigned int cnt = 0;
-    while (flow_cnt_tmp == -1 || (*flow_cnt_ptr_ >= (flow_cnt_tmp + WATER_ON_MIN_TRIG_) && cnt < 10))
-    {
-      water_off_();
-      // wait for valve to close
-      delay(2000);
-      flow_cnt_tmp = *flow_cnt_ptr_;
-      // wait a bit ot be sure water does not flow
-      delay(WATER_ON_MIN_TRIG_DELAY_MSEC_);
-      cnt ++;
-    }
-    // also return number of pulses used to caller
-    *num_of_pulse = cnt;
-    if (*flow_cnt_ptr_ >= (flow_cnt_tmp + WATER_ON_MIN_TRIG_))
-    {
-      // failed to stop water
-      return false;
-    }
-    is_on_ = false;
-    return true;
+    digitalWrite(VALVE_ENABLE, BALL_VALVE_OFF);
   }
 
+  // keep ?
   bool is_on() const
   {
     return is_on_;
@@ -176,32 +56,4 @@ struct valve_t
 
 private:
   bool is_on_{false};
-
-  /** Pointer to variable updated by trigger from water flow counter */
-  uint32_t* flow_cnt_ptr_;
-
-  /** Min number of pulses received to consider water to be ON */
-  const uint32_t WATER_ON_MIN_TRIG_;
-  /** Delay to wait for when checking if water is ON/OFF */
-  const uint32_t WATER_ON_MIN_TRIG_DELAY_MSEC_;
-
-  void water_on_(int pulse_msec=-1)
-  {
-    if (pulse_msec == -1)
-    {
-      pulse_msec = VALVE_PULSE_MSEC;
-    }
-    digitalWrite(VALVE_PHASE, VALVE_PHASE_OPEN);
-    digitalWrite(VALVE_ENABLE, 1);
-    delay(pulse_msec);
-    digitalWrite(VALVE_ENABLE, 0);
-  }
-
-  void water_off_()
-  {
-    digitalWrite(VALVE_PHASE, VALVE_PHASE_CLOSE);
-    digitalWrite(VALVE_ENABLE, 1);
-    delay(VALVE_PULSE_MSEC);
-    digitalWrite(VALVE_ENABLE, 0);
-  }
 };
--- water-control/water-control/water-control.ino
+++ water-control/water-control/water-control.ino
@@ -1,8 +1,8 @@
+#include "ball_valve.h"
 #include "button.h"
 #include "epoch_time_t.h"
 #include "led.h"
 #include "http_reporter.h"
-#include "valve.h"
 #include "web_log.hpp"
 #include "wifi.h"
 
@@ -24,16 +24,6 @@ const unsigned int sec_report_period = (15 * 60); // TODO 15 min
 epoch_time_t last_report_ = -1;
 const unsigned int sec_report_period_when_watering = 60;
 const unsigned int water_loop_period_sec = 5;
-// Max times to try open the valve before giving up
-const unsigned int water_on_max_try = 10;
-// Consider water is on if 1/2 liter has flown, ie ~ 400 pulses / 2
-// TODO clarify that with time waited in valve
-// Currently waiting 10 sec in valve, then check threshold
-// Flow is around 4.5 L / minute (measured)
-// |--> 4.5 / 6 = 0.75 L
-// |--> which is 50% more than threshold
-const unsigned int WATER_ON_MIN_TRIG = (1 / flow_cnt_liter_per_edge) / 2;
-const unsigned int WATER_ON_MIN_TRIG_DELAY_MSEC = 10000;
 
 // Hard limit watering duration, 1.5 hour
 const int64_t MAX_WATER_DURATION_SEC = 5400;
@@ -57,7 +47,7 @@ button_t button;
 epoch_time_sync_t epoch_time_sync;
 led_t led(&epoch_time_sync);
 web_log_t web_log;
-valve_t valve(WATER_ON_MIN_TRIG, WATER_ON_MIN_TRIG_DELAY_MSEC);
+ball_valve_t ball_valve;
 wifi_t wifi;
 http_reporter_t reporter;
 
@@ -95,7 +85,7 @@ void setup()
   epoch_time_sync.init();
   button.init();
   led.init();
-  valve.init(&flow_cnt);
+  ball_valve.init();
 
   // Suspecting low power sleep to make USB serial unstable !!!
   // |--> confirm later, use normal delay/interrupt for now
@@ -134,7 +124,7 @@ report_status sync_with_server(bool allow_clock_adjust)
     next_water_schedule,
     server_cmd.enabled,
     last_water_schedule,
-    valve.is_on(),
+    ball_valve.is_on(),
     epoch_time_sync.uptime_sec(),
     wifi.rssi_dbm(),
     epoch_time_sync.rtc_temp_celsius(),
@@ -201,21 +191,8 @@ void water_for_duration(int32_t duration_sec, bool is_manual_triggered)
   }
   WEB_LOG(String(">> Water for ") + (int)(((float)duration_sec / 60.0) + 0.5) + String(" minutes") + manual_suffix);
   //digitalWrite(LED_BUILTIN, HIGH);
-  // save current flow_cnt, so later we can see if water actually flows
-  uint32_t flow_cnt_tmp = flow_cnt;
-  uint32_t water_on_cnt = 0;
 
-  int pulse_applied_cnt = 0;
-  if (valve.water_on(&pulse_applied_cnt) == false)
-  {
-    WEB_LOG(String("FAILED to start water after ") + pulse_applied_cnt + " pulses... give up...");
-    // FAILURE, set deadline to past, ie water loop won't be done
-    deadline = epoch_time_sync.now();
-  }
-  else
-  {
-    WEB_LOG(String("Started water after ") + pulse_applied_cnt + " pulses");
-  }
+  ball_valve.water_on();
 
   // Report on first iteration
   epoch_time_t http_report_deadline = epoch_time_sync.now();
@@ -257,25 +234,10 @@ void water_for_duration(int32_t duration_sec, bool is_manual_triggered)
     led.fade(water_loop_period_sec, 1000);
   }
 
-  while (valve.water_off(&pulse_applied_cnt) == false)
-  {
-    WEB_LOG(String("FAILED to stop water with" ) + pulse_applied_cnt + " pulses... keep trying...");
-    // Also report here
-    // or else web log will never reach server if stuck in this loop !
-    if (wifi.is_connected())
-    {
-      // HTTP report
-      led.on();
-      report_status rs = sync_with_server(false);
-      led.off();
-    }
-  }
+  ball_valve.water_off();
 
   LOG(" >> STOP Water <<");
   Serial.println("");
-  // delay to ensure log messages are sorted on web
-  delay(2000);
-  WEB_LOG(String(" >> STOP Water << with ") + pulse_applied_cnt + " pulses");
 }
 
 
